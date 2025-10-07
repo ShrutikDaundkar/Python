1d3c244c8380ead50655723154e6848a36fcddcb
ShrutikDaundkar <shrutikdaundkar6225@gmail.com>
2025-10-07 19:06:56 +0530

    update

---
+++ tryout/circle.py
@@ -0,0 +1,6 @@
+class Point:
+    def __init__(self, x ,y):
+        self.X=x
+        self.Y=y
+        
+    def
---
+++ tryout/complex data structure/ipl_teams.py
@@ -0,0 +1,43 @@
+iplteam = {
+    "states": [
+        {
+            "name": "Maharashtra",
+            "team": {
+                "name": "Mumbai Indians",
+                "players": [
+                    {"name": "Rohit Sharma", "role": "Captain"},
+                    {"name": "Hardik Pandya", "role": "Vice Captain"},
+                    {"name": "Suryakumar Yadav", "role": "Batsman"},
+                    {"name": "Pollard", "role": "All-rounder"}
+                ]
+            }
+        },
+        {
+            "name": "Punjab",
+            "team": {
+                "name": "Punjab Super Kings",
+                "players": [
+                    {"name": "Shreyas Iyer", "role": "Captain"},
+                    {"name": "Butler", "role": "Vice Captain"},
+                    {"name": "Prabhsimran", "role": "Batsman"},
+                    {"name": "Stoinis", "role": "All-rounder"}
+                ]
+            }
+        },
+        {
+            "name": "Tamil Nadu",
+            "team": {
+                "name": "Chennai Super Kings",
+                "players": [
+                    {"name": "MS Dhoni", "role": "Captain"},
+                    {"name": "Ruturaj", "role": "Vice Captain"},
+                    {"name": "Rayudu", "role": "Batsman"},
+                    {"name": "Jadeja", "role": "All-rounder"}
+                ]
+            }
+        }
+    ]
+}
+
+#print(iplteam)
+print(iplteam["states"][2]["team"])
